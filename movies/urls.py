1732c6a9a743851767a8cac3214586c9b6c79f10
Maclyn Afonso <>
2020-04-12 18:48:23 +0100

    added functionality to create a new listing

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import home_page
+from .views import create
 
 urlpatterns = [
-    path('', home_page , name = 'home_page')
+#parameter 1 refers to actual url pattern
+#parameter 2 refers to function name in views
+#paramater 'name' should match call in form action
+    path('', home_page , name = 'home_page'),
+    path('create/' , create , name='create')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # import class created in models.py
 from .models import Movie
@@ -11,3 +11,26 @@ def home_page(request):
     stuff_for_frontend = {"search_result" : search_result}
 
     return render(request,'movies/movies_stuff.html',stuff_for_frontend)
+
+def create(request):
+    # import pdb
+    # pdb.set_trace()
+    if request.method == 'POST':
+        data = {
+            'name' : request.POST.get('movie_name'),
+            'picture' : request.POST.get('movie_picture'),
+            'rating' : int(request.POST.get('movie_rating')),
+            'comments' : request.POST.get('movie_comments')
+        }
+        try:
+            response = Movie.objects.create(
+                name = data.get('name'),
+                picture = data.get('picture'),
+                rating = data.get('rating'),
+                comments = data.get('comments')
+            )
+        except Exception as e:
+            print(e)
+            pass
+
+    return redirect('/')
